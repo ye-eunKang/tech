89e63f180818d11355ce75eee95dd6f678f9e756
Kang Yeeun <118848959+ye-eunKang@users.noreply.github.com>
2026-09-23 06:27:52 +0900

    Update display.js

--- public/display.js
+++ public/display.js
@@ -15,8 +15,8 @@
   const videos = {
     1: 'assets/#1_walk.mp4',
     2: 'assets/#4_water',
-    3: 'assets/#4_main1.mp4',
-    4: 'assets/#4_main2.mp4'
+    3: 'assets/#4_main1_low.mp4',
+    4: 'assets/#4_main2_low.mp4'
   };
 
   const socket = new FishSocket({
